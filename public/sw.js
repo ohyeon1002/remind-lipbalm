93495a5fba6acd9993a833faf31213198783473d
ohyeon <skyblueazure4@gmail.com>
2026-02-06 11:49:48 +0900

    push

--- public/sw.js
+++ public/sw.js
@@ -2,6 +2,21 @@ const VERSION = "v1";
 const CACHE_NAME = `remind-lipbalm-${VERSION}`;
 
 self.addEventListener("push", (e) => {
-  const message = e.data.json();
+  if (e.data) {
+    const data = e.data.json();
+    const options = {
+      body: data.body,
+      icon: data.icon || "/bell.svg",
+      badge: "/bell.svg",
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: "2",
+      },
+    };
+    e.waitUntil(self.registration.showNotification(data.title, options));
+  }
 });
 self.addEventListener("fetch", (e) => {});
+self.addEventListener("notificationClick", (e) => {
+  alert("hey!");
+});
