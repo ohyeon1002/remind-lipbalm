993127af1141f60b94ac21d721201c4b245b6421
ohyeon <skyblueazure4@gmail.com>
2026-02-06 11:34:46 +0900

    prisma annotated

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,13 @@
-import { PrismaClient } from "../generated/prisma/client";
+// import { PrismaClient } from "../generated/prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+// const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    datasource: {
-      url: process.env.DATABASE_URL,
-    },
-  });
+// export const prisma =
+//   globalForPrisma.prisma ||
+//   new PrismaClient({
+//     datasource: {
+//       url: process.env.DATABASE_URL,
+//     },
+//   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
