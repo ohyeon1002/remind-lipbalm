d6b3965d416bb22224bd9507bcf6462b24729289
ohyeon <skyblueazure4@gmail.com>
2026-02-06 10:46:11 +0900

    push poc

--- app/[locale]/NotificationButton.tsx
+++ app/[locale]/NotificationButton.tsx
@@ -1,9 +1,25 @@
 "use client";
 import { useTranslations } from "next-intl";
 import { useEffect } from "react";
+// import saveSubscription from "../actions";
 
 function initialNotification() {
   new Notification("Title!!", { body: "body!!" });
+}
+
+function urlBase64ToUint8Array(base64String: string) {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (var i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
 }
 
 export default function NotificationButton() {
@@ -12,10 +28,27 @@ export default function NotificationButton() {
       navigator.serviceWorker.register("/sw.js");
     }
   }, []);
+  const NEXT_PUBLIC_VAPID_PUBLIC_KEY = process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY;
   const t = useTranslations("HomePage");
   const handleRequestPermission = async () => {
     const permission = await Notification.requestPermission();
     if (permission === "granted") {
+      const registration = await navigator.serviceWorker.ready;
+      const subscription = await registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(
+          NEXT_PUBLIC_VAPID_PUBLIC_KEY ? NEXT_PUBLIC_VAPID_PUBLIC_KEY : "",
+        ),
+      });
+      console.log(JSON.stringify(subscription.toJSON()));
+      // try {
+      //   const result = await saveSubscription(subscription.toJSON());
+      //   if (result.sucess) {
+      //     console.log("Saved subscription");
+      //   }
+      // } catch (e) {
+      //   console.error("error!", e);
+      // }
       initialNotification();
     }
   };
---
+++ app/actions.ts
@@ -0,0 +1,4 @@
+// "use sever";
+// export default async function saveSubscription(subsJSON: PushSubscriptionJSON) {
+//   subsJSON.endpoint;
+// }
